b08268451d510633aa26a833e82951ad0ca1ac39
sonigay <43840048+sonigay@users.noreply.github.com>
2020-02-10 02:08:25 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -1435,7 +1435,7 @@ while True:
 						
 
 								await JointheVC(voice_channel, channel)
-								await client.get_channel(msg.channel.id).send('< 거래처 [' + client.get_channel(voice_channel.id).name + '] 이동완료>', tts=False)
+								await client.get_channel(msg.channel.id).send('< ' + client.get_channel(voice_channel.id).name + ' 으로 이동완료 >', tts=False)
 			
 			################ 저장된 정보 초기화 ################
 						
